8e0a6d7a27a6ef24860a0a217b07ea51aa22d212
Nobody <mishasikolenko@mail.ru>
2023-10-01 19:39:00 +0300

    Code clean

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ def __main__():
     username = "_asha.n"
     get_profile_photos(username)
 
-    # for index, url in enumerate(photo_urls, start = 1):
-    #     response = requests.get(url)
-    #     img = Image.open(BytesIO(response.content))
-    #     img.show()
-    #     i = input("AAA: ")
-
 
 if __name__ == "__main__":
     __main__()
